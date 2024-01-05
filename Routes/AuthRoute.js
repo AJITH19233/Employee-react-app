9d3856b880123c673043bb7009d002a9120268b9
AJITH19233 <135130218+AJITH19233@users.noreply.github.com>
2024-01-06 01:37:48 +0530

    Update AuthRoute.js

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -2,8 +2,8 @@ const { Signup, Login } = require('../Controllers/AuthController')
 const router = require('express').Router()
 const {userVerification}=require('../Middlewares/AuthMiddleware')
 
-router.post('/api/signup', Signup)
-router.post('/api/login', Login)
+router.post('/signup', Signup)
+router.post('/login', Login)
 router.post('/',userVerification)
 
 module.exports = router
