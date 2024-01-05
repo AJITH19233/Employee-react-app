d22542f0100f56672b444de7b536d16eb69fb207
AJITH19233 <135130218+AJITH19233@users.noreply.github.com>
2024-01-06 01:39:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,40 +36,24 @@ app.use(express.json());
 app.use("/", authRoute);
 
 
-app.get("/api/get",async(req,res)=>{
+app.get("/get",async(req,res)=>{
   const data = await Employee.find({})
   res.json({success : true , data : data})
 })  
 
 
-//create data || save data in mongodb
-//http://localhost:8080/create
-/*
-{
-  name,
-  email,
-  mobile
-}
-*/
-app.post("/api/create",async(req,res)=>{
+
+app.post("/create",async(req,res)=>{
   console.log(req.body)
   const data = new Employee(req.body)
   await data.save()
   res.send({success : true, message : "data save successfully" , data : data})
 })
 
-//update data 
-// http://localhost:8080/update
-/**
-*  {
-*      id :"",
-*      name : "",
-*      email : "",
-*       moible : ""
-* }
-*/
 
-app.put("/api/update",async(req,res)=>{
+
+
+app.put("/update",async(req,res)=>{
   console.log(req.body)
   const { _id,...rest} = req.body 
 
@@ -80,7 +64,7 @@ app.put("/api/update",async(req,res)=>{
 
 //delete api
 // http://localhost:8080/delete/id
-app.delete("/api/delete/:id",async(req,res)=>{
+app.delete("/delete/:id",async(req,res)=>{
   const id = req.params.id
   console.log(id)
   const data = await Employee.deleteOne({_id : id})
